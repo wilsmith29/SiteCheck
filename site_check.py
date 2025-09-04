4acd81207b16340e303c705c517af2d7fffe85da
William Smith <wilsmith@pdx.edu>
2025-09-04 13:02:10 -0700

    Added functionality for checking website response

--- site_check.py
+++ site_check.py
@@ -1,12 +1,22 @@
 import requests
 from selenium import webdriver
 
-url = input("Copy URL here: ")
-
 def check_site_status(url):
-    pass
+    response = requests.get(url)
+    return response.status_code
 
 def run_tests(url):
     pass
+
+url = input("Copy URL here: ")
+
+status = check_site_status(url)
+
+if 200 <= status < 300:
+   #run_tests(url)
+   print("Website active")
+
+else:
+   print("Website is down, cannot run tests")
 
 
